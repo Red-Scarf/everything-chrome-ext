27148515fe8ced265d71ffb53cebc2d51e2ce598
red_scard <705762646@qq.com>
2023-03-20 14:21:03 +0800

    调整加载配置信息的代码结构

--- js/options.js
+++ js/options.js
@@ -1,28 +1,15 @@
-// 默认配置
-const defaultConfig = {
-	baseUrl: 'http://127.0.0.1'
+const urlTextObj = $('input[name=url]');
+const paramsObj = $('textarea[name=params]');
+// 保存配置
+function saveOptions() {
+	let url = urlTextObj.val();
+	let params = $.parseJSON(paramsObj.val()); // 字符串转成对象直接保存
+	chrome.storage.local.set({ 'baseUrl': url, 'params': params });
 }
-$(function () {
-	let urlTextObj = $('input[name=url]');
-	let paramsObj = $('textarea[name=params]');
-	// 保存按钮
-	$('#save').click(() => {
-		let url = urlTextObj.val();
-		let params = $.parseJSON(paramsObj.val()); // 字符串转成对象直接保存
-		console.log('保存按钮 url', url, params);
-		chrome.storage.local.set({ 'baseUrl': url, 'params': params })
-	})
-	console.log($('.option'));
-	// 配置的回车事件
-	$('.option').keyup((event) => {
-		if (event.keyCode == 13) {
-			$('#save').trigger("click");
-		}
-	})
 
-	// 展示配置信息
+// 展示保存的配置
+function restoreOptions() {
 	chrome.storage.local.get(['baseUrl', 'params']).then((result) => {
-		console.log('获取本地配置信息', result);
 		let defaultConfig = $.defaultConfig();
 		// 默认url
 		let url = defaultConfig.baseUrl;
@@ -35,4 +22,7 @@ $(function () {
 		}
 		paramsObj.val(JSON.stringify(params)); // 转成json字符串才能展示在文本框中
 	})
-})
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+$('#save').click(saveOptions);
