39b6ca9a377b33e82b982829b456252aa543076a
red_scard <705762646@qq.com>
2023-03-24 22:25:14 +0800

    调整默认配置

--- js/config.js
+++ js/config.js
@@ -6,6 +6,9 @@ $.extend({
             params: {
                 'c': 5, // 限制5条
                 'j': 1, // json返回
+                'size_column': 1, // 文件大小
+                'sort': 'size', // 按大小排序
+                'ascending': 0, // 降序
             }
         };
     }
--- js/options.js
+++ js/options.js
@@ -1,16 +1,26 @@
 const urlTextObj = $('input[name=url]');
 const paramsObj = $('textarea[name=params]');
+const defaultConfig = $.defaultConfig();
 // 保存配置
 function saveOptions() {
-	let url = urlTextObj.val();
-	let params = $.parseJSON(paramsObj.val()); // 字符串转成对象直接保存
+	let url = defaultConfig.baseUrl;
+	let params = defaultConfig.params;
+	if (urlTextObj.val()) {
+		url = urlTextObj.val();
+	} else {
+		urlTextObj.val(defaultConfig.baseUrl);
+	}
+	if (paramsObj.val()) {
+		params = $.parseJSON(paramsObj.val()); // 字符串转成对象直接保存
+	} else {
+		paramsObj.val(JSON.stringify(params)); // 赋默认值
+	}
 	chrome.storage.local.set({ 'baseUrl': url, 'params': params });
 }
 
 // 展示保存的配置
 function restoreOptions() {
 	chrome.storage.local.get(['baseUrl', 'params']).then((result) => {
-		let defaultConfig = $.defaultConfig();
 		// 默认url
 		let url = defaultConfig.baseUrl;
 		if (result['baseUrl']) url = result['baseUrl'];
